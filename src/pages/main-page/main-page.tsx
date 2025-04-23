02c1c3ba698cc473b46f0a048c8a622b1c0cc174
alexander_an <web-wild-fox@yandex.ru>
2025-04-23 14:10:59 +0400

    Добавляет компоненту "Сортировка" логику + рефакторинг

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,7 +2,11 @@ import {useState} from 'react';
 import {Helmet} from 'react-helmet-async';
 import {useAppDispatch, useAppSelector} from '@/hooks';
 import {cityСhange} from '@/store/action';
-import {ClassByTypeCard} from '@/constants';
+import {
+  ClassByTypeCard,
+  SortingType,
+  DEFAULT_SORTING_TYPE
+} from '@/constants';
 import Header from '@/components/header/header';
 import NavList from '@/components/nav-list/nav-list';
 import PlacesSorting from '@/components/places-sorting/places-sorting';
@@ -11,27 +15,65 @@ import Map from '@/components/map/map';
 
 export default function MainPage(): JSX.Element {
   const [selectedPointId, setSelectedPointId] = useState<string | undefined>(undefined);
+  const [currentSorting, setCurrentSortingType] = useState<string>(DEFAULT_SORTING_TYPE);
+  const [isActive, setIsActive] = useState<boolean>(false);
 
   const currentCity = useAppSelector((state) => state.city);
   const offers = useAppSelector((state) => state.offers);
   const dispatch = useAppDispatch();
 
-  const getSelectedPointId = (id: string | null) => {
-    setSelectedPointId(
-      typeof id === 'string' ? id : undefined
-    );
+  const getSelectedPointId = (id: string | undefined) => {
+    setSelectedPointId(id ? id : undefined);
   };
 
   const getFilterOffers = (city: string) => offers.filter(
     (offer) => offer.city.name === city
   );
 
-  const handleCityChangeClick = (city: string): void => {
-    dispatch(cityСhange(city));
+  const filteredOffers = getFilterOffers(currentCity);
+  const isOffersList = filteredOffers[0];
+
+  const typesSort = {
+    [SortingType.priceUp]:[...filteredOffers].sort(
+      (a, b) => a.price - b.price),
+    [SortingType.priceDown]:[...filteredOffers].sort(
+      (a, b) => b.price - a.price),
+    [SortingType.rating]:[...filteredOffers].sort(
+      (a, b) => b.rating - a.rating),
+    [SortingType.popular]: [...filteredOffers],
   };
 
-  const offersForCity = getFilterOffers(currentCity);
-  const isOffersList = offersForCity[0];
+  const getSortedOffers = (sort: string) => {
+    switch (sort) {
+      case SortingType.popular:
+        return typesSort[sort];
+      case SortingType.priceUp:
+        return typesSort[sort];
+      case SortingType.priceDown:
+        return typesSort[sort];
+      case SortingType.rating:
+        return typesSort[sort];
+      default:
+        return filteredOffers;
+    }
+  };
+
+  const sortedOffers = getSortedOffers(currentSorting);
+
+  const handleSortingChangeClick = (type: string) => {
+    setCurrentSortingType(type);
+  };
+
+  const handleSortingViewClick = () => {
+    setIsActive((boolean) => !boolean);
+  };
+
+  const handleCityChangeClick = (city: string): void => {
+    setCurrentSortingType(DEFAULT_SORTING_TYPE);
+    setIsActive(false);
+
+    dispatch(cityСhange(city));
+  };
 
   return (
     <div className="page page--gray page--main">
@@ -56,14 +98,19 @@ export default function MainPage(): JSX.Element {
               <h2 className="visually-hidden">Places</h2>
 
               <b className="places__found">
-                {offersForCity.length} places to stay in {currentCity}
+                {filteredOffers.length} places to stay in {currentCity}
               </b>
               {
                 isOffersList && (
                   <>
-                    <PlacesSorting />
+                    <PlacesSorting
+                      isActive={isActive}
+                      currentSorting={currentSorting}
+                      onSortingChange={handleSortingChangeClick}
+                      onSortingView={handleSortingViewClick}
+                    />
                     <OffersList
-                      offers={offersForCity}
+                      offers={sortedOffers}
                       cardClassName={ClassByTypeCard.MainPageCardType}
                       onCardAction={getSelectedPointId}
                     />
@@ -76,8 +123,8 @@ export default function MainPage(): JSX.Element {
                 <div className="cities__right-section">
                   <section className="cities__map map">
                     <Map
-                      points={offersForCity}
-                      startPoint={offersForCity[0].city}
+                      points={filteredOffers}
+                      startPoint={filteredOffers[0].city}
                       selectedPointId={selectedPointId}
                     />
                   </section>
